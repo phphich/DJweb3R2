3da22020d5022ffd2966da06762c252e13ff33fb
Phitchayaphak Phiphitphatphaisit <30717707+phphich@users.noreply.github.com>
2023-01-18 13:03:35 +0700

    Update urls.py

--- DJweb3R2/urls.py
+++ DJweb3R2/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('productapp/', include('ProductApp.urls')),
     path('profileapp/', include('ProfileApp.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
